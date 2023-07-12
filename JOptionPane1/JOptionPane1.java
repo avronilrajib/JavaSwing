8930215aa38712e38ea2db253f52dc2987ba98d7
avronil rajib <110673121+avronilrajib@users.noreply.github.com>
2023-07-12 19:54:23 +0600

    Show Massage Dialog

---
+++ JOptionPane1/JOptionPane1.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class JOptionPane1{
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Hello");
+        JOptionPane.showConfirmDialog(null, "Hello Bangladesh", null, 0, 0, null);
+    }
+}
